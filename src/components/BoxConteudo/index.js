3c59cc58c14f6b31830faf6789426b096d227552
shl0mo <salomao.dacostacruz@gmail.com>
2021-02-03 14:51:48 -0400

    padding_bottom-alternativa

--- src/components/BoxConteudo/index.js
+++ src/components/BoxConteudo/index.js
@@ -4,7 +4,7 @@ export const BoxConteudo = styled.div`
 	background-color: white;
 	width: 65vw;
 	min-height: 100px;
-	padding: 60px 80px;
+	padding: 60px 80px 80px 80px;
 	border: 1px solid rgba(0,0,0,0.2);
 	text-align: justify;
 	font-size: 1.38em;
