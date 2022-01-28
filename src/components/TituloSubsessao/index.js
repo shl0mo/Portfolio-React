f72edde1d827e443e4fb7d0f8873fb7fc26cd486
shl0mo <salomao.dacostacruz@gmail.com>
2022-01-28 07:29:25 -0400

    ajustes-2

--- src/components/TituloSubsessao/index.js
+++ src/components/TituloSubsessao/index.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 const TituloSubsessaoStyled = styled.h3`
 	margin: ${props => props.primeiro ? '0 0 20px 0' : '40px 0 20px 0'};
+	cursor: default;
 }`
 
 export const TituloSubsessao = ({ children, primeiro }) => {
